1d14be3fb82a8dd96d2677875fd2d0d284380c59
Bruno Nunes <silva.nunes@gmail.com>
2018-08-31 16:58:46 -0300

    add inteligidea

---
+++ IdeaProjects/WebDriverJava/src/test/java/Tests/InformacaoUsuarioTest.java
@@ -0,0 +1,16 @@
+package Tests;
+
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class InformacaoUsuarioTest {
+    @Test
+    public void testAdicionarInformacaoUsuario() {
+      System.setProperty("webdriver.chrome.driver","C:\\Users\\Ribeiro Nunes\\Drivers\\chromedriver.exe");
+        WebDriver navegador = new ChromeDriver();
+     // assertEquals( expected: 1, actual: 1);
+    }
+}
